4554dddc4f25cc719df44bc156e0e34733a4d279
ZackRohde <zdrohde@outlook.com>
2022-11-29 11:30:32 -0800

    started the conditional statement for each scenario and returning the win

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Pseudocode:
 // create a function named getComputerChoice 
 // set a variable with the choices of the game outside of the function
-// create a Math.floor(Math.random()) of the length of the variable for the choices
-// 
+// create a Math.floor(Math.random()) of the length of the variable for the choices and set it to a variable 
+// return the gameMoveChoices with the randomRoll in brackets because it's an array
 
 const gameMoveChoices = ["rock", "paper", "scissors"]
 
@@ -11,3 +11,16 @@ const randomRoll = Math.floor(Math.random() * gameMoveChoices.length)
   return gameMoveChoices[randomRoll]
 }
 console.log(getComputerChoice())
+
+// Pseudocode:
+// create a function named singleRound that takes in two parameters (playerSelection, computerSelection)
+// create a conditional statement going over each scenario and returning the winner or loser 
+
+const singleRound = (playerSelection, computerSelection) => {
+  if (playerSelection === gameMoveChoices[0] && computerSelection === gameMoveChoices[2]) {
+    return "Rock beats scissors! You win!"
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "Paper beats rock! You win!"
+  }
+}
+console.log(singleRound("paper", "rock"))
